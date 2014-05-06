c3096dfd0134ae41e0932b086b218aad8a4082e4
Dave King <dave@davbo.org>
2014-05-06 22:44:43 +0100

    Test binding locally and listen for all messages

--- src/test.rs
+++ src/test.rs
@@ -2,9 +2,13 @@ extern crate ssdp;
 extern crate native;
 
 use native::io::net::UdpSocket;
-use ssdp::{SSDPListener, SSDP_MULTICAST_SOCKET};
+use ssdp::{SSDPListener};
+
+use std::io::net::ip::{SocketAddr, IpAddr, Ipv4Addr};
+static LOCAL_SOCK : SocketAddr = SocketAddr{ip: Ipv4Addr(0,0,0,0), port: 1900};
 
 fn main() {
-    let mut listener : UdpSocket = UdpSocket::bind(SSDP_MULTICAST_SOCKET).unwrap();
+    let mut listener : UdpSocket = UdpSocket::bind(LOCAL_SOCK).unwrap();
     listener.listen();
+    listener.send_discover();
 }
